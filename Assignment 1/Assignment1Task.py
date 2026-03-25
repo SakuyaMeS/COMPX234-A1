b134b9895de0d74351f75500545fca16bb3bd4f2
SakuyaMeS <2952704112@qq.com>
2026-03-25 08:29:10 +0800

    add semaphores for task 2 synchronization

--- Assignment 1/Assignment1Task.py
+++ Assignment 1/Assignment1Task.py
@@ -20,6 +20,9 @@ class Assignment1:
         self.mThreads = []             # list for machine threads
         self.pThreads = []             # list for printer threads
 
+        self.semaphore = threading.Semaphore(self.NUM_PRINTERS)
+        self.binary = threading.Semaphore(1)
+
     def startSimulation(self):
         # Create Machine and Printer threads
         # Write code here
